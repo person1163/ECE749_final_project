1436ffc7cc5d5d7cfd024fc4e656124533569e44
Vaibhavta15 <90544184+Vaibhavta15@users.noreply.github.com>
2025-12-02 20:05:45 -0500

    final

--- project_benches/lc3/sim/regress.sh
+++ project_benches/lc3/sim/regress.sh
@@ -1,5 +1,5 @@
 rm *.ucdb
-make cli TEST_NAME=test_top CODE_COVERAGE_ENABLE=1 UVM_VERBOSITY=UVM_LOW BATCH_DO_COMMANDS='-do "set NoQuitOnFinish 1; run -all; coverage attribute -name TESTNAME -value $(TEST_NAME).$(TEST_SEED); coverage save $(TEST_NAME).$(TEST_SEED).ucdb; quit -f"'
-cp transcript transcript_test_top
-make merge_coverage
+module load questa
+make clean
+make cli TEST_NAME=test_top TEST_SEED=2900320262
 make view_coverage
